7a0ed451d97499c339907ede832fd2b25bfe00e0
grind-t <grindt.gd@gmail.com>
2021-07-19 17:23:10 +0300

    improve tests

--- src/index.spec.tsx
+++ src/index.spec.tsx
@@ -1,52 +1,76 @@
 import React from 'react';
-import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import {
+  fireEvent,
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import AnimatedRoute from './index';
 import { BrowserRouter, Link } from 'react-router-dom';
 import { LastLocationProvider } from 'react-router-last-location';
 
-describe('sample desc', () => {
-  it('sample it', async () => {
-    render(
-      <BrowserRouter>
-        <LastLocationProvider>
-          <Link to="/a">Link a</Link>
-          <Link
-            to={{
-              pathname: '/b',
-              state: { transition: { classNames: 'slide' } },
-            }}
-          >
-            Link b
-          </Link>
-          <Link to="/с">Link c</Link>
+type ProvidersProps = { children: React.ReactNode };
+const Providers = ({ children }: ProvidersProps) => (
+  <BrowserRouter>
+    <LastLocationProvider>{children}</LastLocationProvider>
+  </BrowserRouter>
+);
 
-          <AnimatedRoute classNames="fade" timeout={1000} path="/a">
-            <span>a</span>
-          </AnimatedRoute>
-          <AnimatedRoute classNames="fade" timeout={1000} path="/b">
-            <span>b</span>
-          </AnimatedRoute>
-          <AnimatedRoute classNames="fade" timeout={1000} path="/с">
-            <span>c</span>
-          </AnimatedRoute>
-        </LastLocationProvider>
-      </BrowserRouter>
-    );
-    const linkA = screen.getByText('Link a');
-    const linkB = screen.getByText('Link b');
-    const linkC = screen.getByText('Link c');
-    fireEvent.click(linkA);
-    expect(screen.getByText('a')).toBeInTheDocument();
-    fireEvent.click(linkB);
-    expect(screen.getByText('b')).toBeInTheDocument();
-    fireEvent.click(linkC);
-    expect(screen.getByText('c')).toBeInTheDocument();
-    screen.debug();
-    /*
-    await waitFor(() => {
-      expect(screen.queryByText('a')).not.toBeInTheDocument();
-    });
-    screen.debug();*/
-  });
+it('should unmount on exit by default', async () => {
+  render(
+    <Providers>
+      <Link to="/other" />
+      <AnimatedRoute timeout={10} exact path="/">
+        <div>root</div>
+      </AnimatedRoute>
+    </Providers>
+  );
+  expect(screen.getByText('root')).toBeInTheDocument();
+  fireEvent.click(screen.getByRole('link'));
+  await waitForElementToBeRemoved(() => screen.queryByText('root'));
+});
+
+it('should override transition', async () => {
+  render(
+    <Providers>
+      <Link
+        to={{
+          pathname: '/a',
+          state: { transition: { classNames: 'slide' } },
+        }}
+      />
+
+      <AnimatedRoute classNames="fade" timeout={10} path="/a">
+        <div>a</div>
+      </AnimatedRoute>
+    </Providers>
+  );
+  fireEvent.click(screen.getByRole('link'));
+  expect(screen.getByText('a')).toHaveClass('slide-enter slide-enter-active');
+});
+
+it("shouldn't override active exit transition", () => {
+  render(
+    <Providers>
+      <Link to="/a" />
+      <Link
+        to={{
+          pathname: '/b',
+          state: { transition: { classNames: 'slide' } },
+        }}
+      />
+      <Link to="/c" />
+
+      <AnimatedRoute classNames="fade" timeout={10} path="/a">
+        <div>a</div>
+      </AnimatedRoute>
+      <AnimatedRoute classNames="fade" timeout={10} path="/b">
+        <div>b</div>
+      </AnimatedRoute>
+    </Providers>
+  );
+  screen.getAllByRole('link').forEach((link) => fireEvent.click(link));
+  expect(screen.getByText('a')).toHaveClass('fade-exit fade-exit-active');
+  expect(screen.getByText('b')).toHaveClass('slide-exit slide-exit-active');
 });
